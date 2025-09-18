5733d47dc021f414d7525a8026ce920c64ba4e6f
Gabriel A. Mantegna <gm1710@della8.princeton.edu>
2025-09-18 18:45:26 -0400

    cluster changes

--- src/jobscript.sh
+++ src/jobscript.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=ldes         # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=4        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --cpus-per-task=16        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=16GB         # memory per cpu-core (4G is default)
 #SBATCH --time=23:59:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=end,fail          # send email when job ends
